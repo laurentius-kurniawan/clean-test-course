00dca707db0b0ab8762183c3ae5249470490a7d4
Laurentius Adi <laurentius.kurniawan@yahoo.com>
2026-08-11 21:40:32 +1000

    added and automated unit tests

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -1,35 +1,40 @@
 from api.controllers import Delivery
 from django_mock_queries.query import MockSet, MockModel
 
+
 def test_LotsOfItems():
-  #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
-  delivery_distance = 6
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
-  assert cost == 7.5
+    # Arrange
+    order = MockSet()
+    order.add(MockModel(quantity=5))
+    order.add(MockModel(quantity=5))
+    order.add(MockModel(quantity=5))
+    delivery_distance = 6
+    # Act
+    cost = Delivery.calculate(order, delivery_distance)
+    # Assert
+    assert cost == 7.5
+
 
 def test_MiddleOfTheRoadItems():
-  #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
-  delivery_distance = 4
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
-  assert cost == 5
+    # Arrange
+    order = MockSet()
+    order.add(MockModel(quantity=2))
+    order.add(MockModel(quantity=2))
+    order.add(MockModel(quantity=2))
+    delivery_distance = 4
+    # Act
+    cost = Delivery.calculate(order, delivery_distance)
+    # Assert
+    assert cost == 5
+
 
 def test_LittleItems():
-  #Arrange
-  # TODO: Arrange the items to run the test
-  #Act
-  # TODO: Call the function that will be tested
-  #Assert
-  # TODO: replace the pass with an assert to test the value returned.
-  pass
+    # Arrange
+    order = MockSet()
+    order.add(MockModel(quantity=3))
+    order.add(MockModel(quantity=1))
+    del_dist = 2
+    # Act
+    cost = Delivery.calculate(order, del_dist)
+    # Assert
+    assert cost == 2.50
