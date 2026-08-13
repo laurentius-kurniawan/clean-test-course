25857d0be0ef39a74f09e80297d5b75afaf2a649
Laurentius Adi <laurentius.kurniawan@yahoo.com>
2026-08-13 21:56:51 +1000

    Lesson 6 Upload Video

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,35 @@
+//describe('Shows Appeteasers', () => {
+//it('Connect to Dev Server', () => {
+//cy.visit('https://hangryhippo.quantic.host/');
+//});
+// it('selects Handhelds', () => {
+//cy.contains('Handhelds').click();
+//cy.contains('Cheese Burger');
+//cy.contains('Fajita Tacos');
+//});//
+//it('selects Appeteasers', () => {
+//TODO: Add a test to check Appeteasers
+//cy.contains('Appeteasers').click();
+//cy.contains('Tater Tots');
+//cy.contains('Cheese Burger').should('not.exist');
+//cy.contains('Fajita Tacos').should('not.exist');
+//});
+//}); //
+
 describe('Shows Appeteasers', () => {
-  it('Connect to Dev Server', () => {
+  it('navigates menu categories and verifies items', () => {
+    // 1. Visit page
     cy.visit('https://hangryhippo.quantic.host/');
-  });
-  it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
-  });
-  it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+
+    // 2. Test Handhelds
+    cy.contains('Handhelds').click();
+    cy.contains('Cheese Burger').should('be.visible');
+    cy.contains('Fajita Tacos').should('be.visible');
+
+    // 3. Test Appeteasers
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots').should('be.visible');
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
